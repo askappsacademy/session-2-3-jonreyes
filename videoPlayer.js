d5b9ab50a99ad9be38f223dc89cc0a696c5c1fb6
jonxreyes <jonrey3@gmail.com>
2020-09-17 02:09:39 +0000

    First Submission

--- videoPlayer.js
+++ videoPlayer.js
@@ -8,9 +8,36 @@ const videoArray = [
 ];
 
 // task 1 - declare a var to target video play list element
+let videoList = document.getElementById('videoList').children;
 
 // task 2 - write a function setVideos where it loops through the video play list elements in index.html file then apply click event handler to pass url from videoArray to another function
+let setVideos = (v)=>{
+  for(let i=0; i<v.length;i++){
+    v[i].onclick = ()=>{
+      url = videoArray[i]
+      loadVideo(url);
+    }
+  }
+}
 
 // task 3 - write a function loadVideo where it takes an url as a parameter to render the video as an iframe in the video container element
+let loadVideo = (url)=>{
+  let video = document.getElementById('video');
+  let videoPlayer = document.createElement('iframe');
+  videoPlayer.height = video.getBoundingClientRect().height;
+  videoPlayer.width = video.getBoundingClientRect().width;
+  videoPlayer.src = url;
+  if(video.children.length==0){
+    video.appendChild(videoPlayer)
+  } else {
+    prev = video.children[0].src;
+    cur = videoPlayer.src;
+    if(prev!=cur){
+      video.removeChild(video.children[0]);
+      video.appendChild(videoPlayer);
+    }
+  }
+}
 
 // task 4 - call setVideos function to pass in the above video play list element
+setVideos(videoList);
